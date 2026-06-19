c7395bc9aea35078505d715d3fdeb7525eca9591
Nivedita Singh <niveditareenasingh@gmail.com>
2026-06-19 12:19:28 +0530

    Add operator examples in Python

---
+++ OPERATORS/Bitwise_Operators.py
@@ -0,0 +1,9 @@
+a = 5      # 101
+b = 3      # 011
+
+print(a & b)   # 1  (001)
+print(a | b)   # 7  (111)
+print(a ^ b)   # 6  (110)
+print(~a)      # -6
+print(a << 1)  # 10 (1010)
+print(a >> 1)  # 2  (10)
---
+++ OPERATORS/Logical_Operators.py
@@ -0,0 +1,13 @@
+age = 20
+has_id = True
+
+print(age >= 18 and has_id)
+
+is_weekend = False
+is_holiday = True
+
+print(is_weekend or is_holiday)
+
+is_raining = False
+
+print(not is_raining)
